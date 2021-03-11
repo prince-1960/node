73c8586768a39f929c0a4d6e67fb89e5671787b0
prince-1960 <princesingh1960@gmail.com>
2021-03-12 01:48:35 +0530

    new checks

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello I'm a CI CD Pipeline');
+  res.send('Hello Im a CI CD Pipeline');
 });
 
 app.listen(PORT);
